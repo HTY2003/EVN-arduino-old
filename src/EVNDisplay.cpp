e7d28d709425a05cf300e8819a6a8ef437498feb
HTY2003 <randumbperson@gmail.com>
2023-12-14 00:58:11 +0800

    remove bulky u8g2 object from evndisplay

--- src/EVNDisplay.cpp
+++ src/EVNDisplay.cpp
@@ -3,16 +3,12 @@
 EVNDisplay::EVNDisplay(uint8_t port, bool rotate)
 {
     _port = constrain(port, 1, 16);
+
+    //use diff i2c bus depending on port
     if (_port > 8)
-    {
-        _display = new U8G2_SSD1306_128X64_NONAME_F_2ND_HW_I2C(U8G2_R0, U8X8_PIN_NONE);
         _display8x8 = new U8X8_SSD1306_128X64_NONAME_2ND_HW_I2C(U8X8_PIN_NONE);
-    }
     else
-    {
-        _display = new U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0, U8X8_PIN_NONE);
         _display8x8 = new U8X8_SSD1306_128X64_NONAME_HW_I2C(U8X8_PIN_NONE);
-    }
     _rotate = rotate;
 }
 
@@ -20,16 +16,18 @@ void EVNDisplay::begin()
 {
     uint8_t prev_port = EVNAlpha::sharedPorts().getPort();
     EVNAlpha::sharedPorts().setPort(_port);
-    _display->begin();
+
+    //init, set font, wipe prev display
     _display8x8->begin();
     _display8x8->setFont(u8x8_font_pxplusibmcgathin_f);
-
     this->clear();
 
+    //flip if needed
     if (_rotate)
     {
         _display8x8->setFlipMode(_rotate);
     }
+
     EVNAlpha::sharedPorts().setPort(prev_port);
 }
 
@@ -37,11 +35,14 @@ void EVNDisplay::clear()
 {
     uint8_t prev_port = EVNAlpha::sharedPorts().getPort();
     EVNAlpha::sharedPorts().setPort(_port);
+
+    //wipe display
     _display8x8->clearDisplay();
-    for (int i = 0; i < NO_OF_ROWS; i++)
-    {
+    //erase all stored row names
+    for (int i = 0; i < 8; i++)
         _rownamelen[i] = 0;
-    }
+
+
     EVNAlpha::sharedPorts().setPort(prev_port);
 }
 
@@ -50,8 +51,13 @@ void EVNDisplay::clearLine(uint8_t row)
     uint8_t prev_port = EVNAlpha::sharedPorts().getPort();
     EVNAlpha::sharedPorts().setPort(_port);
     uint8_t rowc = constrain(row, 0, NO_OF_ROWS - 1);
+
+    //empty line on display
     _display8x8->clearLine(rowc);
+
+    //erase stored row name
     _rownamelen[rowc] = 0;
+
     EVNAlpha::sharedPorts().setPort(prev_port);
 }
 
@@ -59,8 +65,13 @@ void EVNDisplay::rotate()
 {
     uint8_t prev_port = EVNAlpha::sharedPorts().getPort();
     EVNAlpha::sharedPorts().setPort(_port);
+
+    //flip the rotation the display is set to
     _rotate = !_rotate;
+
+    //and write it to the display
     _display8x8->setFlipMode(_rotate);
+
     EVNAlpha::sharedPorts().setPort(prev_port);
 }
 
@@ -68,8 +79,30 @@ void EVNDisplay::splashEVN()
 {
     uint8_t prev_port = EVNAlpha::sharedPorts().getPort();
     EVNAlpha::sharedPorts().setPort(_port);
-    _display->clearBuffer();
-    _display->drawXBM(round((SCREEN_WIDTH - LOGO_WIDTH) / 2), round((SCREEN_HEIGHT - LOGO_HEIGHT) / 2), LOGO_WIDTH, LOGO_HEIGHT, logo);
-    _display->sendBuffer();
+
+    //clear display
+    this->clear();
+
+    //draw logo line by line
+    for (int y = 0; y < (NO_OF_ROWS / 2); y++)
+    {
+        for (int x = 0; x < MAX_CHAR; x++)
+        {
+            uint8_t start_tile[8] = { 0 }, end_tile[8] = { 0 };
+            for (int i = 0; i < 8; i++)
+            {
+                start_tile[i] = logo[y * SCREEN_WIDTH + x * 8 + i];
+                end_tile[i] = logo[(NO_OF_ROWS - 1 - y) * SCREEN_WIDTH + (MAX_CHAR - 1 - x) * 8 + i];
+            }
+            _display8x8->drawTile(x, y, 1, start_tile);
+            _display8x8->drawTile(MAX_CHAR - 1 - x, NO_OF_ROWS - 1 - y, 1, end_tile);
+            delayMicroseconds(15625 * 2);
+        }
+        // uint8_t tile[SCREEN_WIDTH] = { 0,0,0,0,0,0,0,0 };
+        // for (int i = 0; i < SCREEN_WIDTH; i++)
+        //     tile[i] = logo[y * SCREEN_WIDTH + i];
+        // _display8x8->drawTile(0, y, MAX_CHAR, tile);
+    }
+
     EVNAlpha::sharedPorts().setPort(prev_port);
 }
